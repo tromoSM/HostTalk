9fbdb54113996af3ebcf891c539eedc3fa562377
tromoSM <tromosm7@gmail.com>
2026-02-03 01:55:17 +0530

    static/chat.js

--- static/chat.js
+++ static/chat.js
@@ -6,24 +6,35 @@ window.addEventListener("DOMContentLoaded",function(){
         usrahh=prompt("Enter username to continue")
         localStorage.setItem("userID",usrahh)
     }
+ const orgAud=this.window.Audio
+  window.Audio=function(...args){
+  const audio=new orgAud(...args)
+  audio.volume=0.3
+  return audio
+  }
     sdihkid.emit('join',{name:usrahh})
     sdihkid.on("mainchat",d=>{
+    notificationA=new Audio(`static/recieved.mp3`)
+    notificationB=new Audio(`static/sent.mp3`)
+    
         const each$$cov=document.createElement("div")
         const each$$msg=document.createElement("p")
         each$$msg.innerHTML=`${d.msg}`
-        if(d.msg.length>=62){
-            each$$msg.setAttribute('large','message')
+        if(d.msg.length>=31){
+            each$$msg.setAttribute('large','2')
+            each$$cov.setAttribute('large','2c')
         }
         if(d.name==usrahh){
             each$$msg.setAttribute('mine','yesvro')
+            each$$cov.setAttribute('mine','yescover')
             each$$cov.setAttribute('setmarg','02')
         }
         else{
             let a=document.createElement('p')
             a.innerHTML=d.name
             a.setAttribute('tag','user')
-            notiff$osund=new Audio(`static/pulse.mp3`)
             each$$msg.setAttribute('mine','fuhnaw')
+            each$$cov.setAttribute('mine','nocover')
             each$$cov.appendChild(a)
         }
         each$$cov.setAttribute("bubble","cover")
@@ -36,8 +47,10 @@ window.addEventListener("DOMContentLoaded",function(){
        if(inp.value.trim()!==""){
         sdihkid.emit("mainchat",inp.value)
         inp.value=""
+        notificationB.play() 
         window.scrollTo(0,document.body.scrollHeight);
        }
+
     }
     document.querySelector('[dih="yo"]').addEventListener('keydown',function(dih){
         if(dih.key=="Enter"){
