3ccab3834fd3aeed7e6c6d92c0059e6df83ad8bd
tromoSM <tromosm7@gmail.com>
2026-02-03 01:59:13 +0530

    static/options.js

--- static/options.js
+++ static/options.js
@@ -5,15 +5,29 @@ window.addEventListener("DOMContentLoaded",function(){
         })
     })
     refreshselected()
+    //checkopt
+    let b=localStorage.getItem('app')
+    if(b){
+        refreshselected()
+        if(b=="dark"){
+            document.documentElement.setAttribute('dark','true')
+        }
+        else{
+            document.documentElement.setAttribute('dark','fuhnaw') 
+        }
+    }
+    //no checkin on loader below yo. sybau
     document.querySelectorAll('[action]').forEach(each=>{
         each.setAttribute('selected','none')
       each.addEventListener('click',function(){
         let a=each.getAttribute('action')
         if(a=="darkmode"){
             document.documentElement.setAttribute('dark','true')
+            localStorage.setItem('app','dark')
         }
         else if(a=="lightmode"){
             document.documentElement.setAttribute('dark','fuhnaw') 
+            localStorage.setItem('app','fuhnaw')
         }
         else if(a=="showset"){
             document.querySelector(`[visible='settings']`).style.display='flex'
